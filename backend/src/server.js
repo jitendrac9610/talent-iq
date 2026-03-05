a8439e87d1927a4a80f7e260a8df37e3eec89bb9
jitendrac9610 <163170290+jitendrac9610@users.noreply.github.com>
2026-03-06 01:22:47 +0530

    Refactor server routes and static file handling
    Removed the default root route and updated static file serving logic for production. Adjusted server listening port to default to 3000 if not specified.

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,18 +6,16 @@ const app = express();
 
 const __dirname = path.resolve();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello, World!" });
-});
-
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  const frontendPath = path.join(__dirname, "../frontend/dist");
+
+  app.use(express.static(frontendPath));
 
   app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server is running on port ${ENV.PORT} `);
+app.listen(ENV.PORT || 3000, () => {
+  console.log(`Server is running on port ${ENV.PORT}`);
 });
